ba36f57b3f140b7dbbaa2354f34dec650b85b6c7
DW2733 <dw2733@tc.columbia.edu>
2017-04-04 08:53:19 -0400

    master5

--- app/src/main/java/com/example/dadriaunna01/inclassassignment08_dadriaunnaw3/Students.java
+++ app/src/main/java/com/example/dadriaunna01/inclassassignment08_dadriaunnaw3/Students.java
@@ -5,4 +5,47 @@ package com.example.dadriaunna01.inclassassignment08_dadriaunnaw3;
  */
 
 public class Students {
+
+    private boolean passing;
+    private String name;
+    private int age;
+
+    public Students(boolean passing, String name, int age) {
+        this.passing = passing;
+        this.name = name;
+        this.age = age;
+    }
+
+    public boolean isPassing() {
+        return passing;
+    }
+
+    public void setPassing(boolean passing) {
+        this.passing = passing;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Students{" +
+                "passing=" + passing +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
